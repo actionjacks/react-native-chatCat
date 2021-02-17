6dc96cef866cee76a8b32d7d65d00d6f9adfa74c
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-02-17 23:41:06 +0100

    state for handle add chat room

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Button, Input } from "react-native-elements";
 
 const AddChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
+
+  useLayoutEffect(() => {}, []);
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Input
+        placeholder="enter chat room name"
+        value={input}
+        onChangeText={(text) => setInput(text)}
+      />
     </View>
   );
 };
 
 export default AddChatScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
