fad3798f955e7079b78bda4e60f8cc4a49e63bc8
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-02-19 21:44:42 +0100

    add key for view sender

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -134,7 +134,7 @@ const ChatScreen = ({ navigation, route }) => {
                     <Text style={styles.recieverText}>{data.message}</Text>
                   </View>
                 ) : (
-                  <View style={styles.sender}>
+                  <View key={id} style={styles.sender}>
                     <Avatar
                       rounded
                       size={30}
